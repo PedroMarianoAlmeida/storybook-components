3bd4fc7726a301d66721505f509602ea29769078
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-08-31 00:33:12 -0300

    FilterByTextComponent starts working

---
+++ src/components/find-itens-by-text/FindItensByText.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import FindItensByText from './FindItensByText';
+
+export default {
+    title: 'Filter/Find Itens By Text',
+    component: FindItensByText
+}
+
+export const Logic = () => <FindItensByText placeHolder='Type expression to search'/>
