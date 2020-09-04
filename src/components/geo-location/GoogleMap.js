9c3634042d81e66bf2d43e6fa1c2bd16b76516d3
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-09-03 21:01:20 -0300

    Map working with API Key

--- src/components/geo-location/GoogleMap.js
+++ src/components/geo-location/GoogleMap.js
@@ -15,7 +15,7 @@ const GoogleMapComponent = () => {
     return (
         <div style={{ height: `500px`, width:'1000px' }}>
             <WrappedMap
-                googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `400px` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
