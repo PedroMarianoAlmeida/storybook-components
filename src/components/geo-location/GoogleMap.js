fa956059a98283a40da5303e0b7657c024b90063
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-09-04 11:33:04 -0300

    Another map style implemented

--- src/components/geo-location/GoogleMap.js
+++ src/components/geo-location/GoogleMap.js
@@ -9,7 +9,11 @@ const toMarker = [
     {lat: 43.651070, lng: -79.347015, title:'Toronto', description:'Capital city of the Canadian province of Ontario'},
 ]
 
-const lostInDesertStyle = [
+//Source to styles: https://snazzymaps.com/
+const mapStyles = {
+    default: '',
+    
+    lostInDesert: [
     {
         "elementType": "labels",
         "stylers": [
@@ -209,7 +213,146 @@ const lostInDesertStyle = [
     {},
     {},
     {}
-]
+    ],
+
+    modest: [
+        {
+            "featureType": "all",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#202c3e"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "gamma": 0.01
+                },
+                {
+                    "lightness": 20
+                },
+                {
+                    "weight": "1.39"
+                },
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "weight": "0.96"
+                },
+                {
+                    "saturation": "9"
+                },
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#000000"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "lightness": 30
+                },
+                {
+                    "saturation": "9"
+                },
+                {
+                    "color": "#29446b"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "saturation": 20
+                }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "lightness": 20
+                },
+                {
+                    "saturation": -20
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "lightness": 10
+                },
+                {
+                    "saturation": -30
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#193a55"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "saturation": 25
+                },
+                {
+                    "lightness": 25
+                },
+                {
+                    "weight": "0.01"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "lightness": -20
+                }
+            ]
+        }
+    ],
+}
 
 const Map = () => {
     const [ selectedMarker, setSelectedMarker ] = useState(null);
@@ -218,7 +361,7 @@ const Map = () => {
         <GoogleMap 
             defaultZoom={10}
             defaultCenter={{ lat: 49.3023, lng:  -123.107 }}
-            defaultOptions={{styles: lostInDesertStyle}}
+            defaultOptions={{styles: mapStyles.lostInDesert}}
         >
             {toMarker.map(point => (
                 <Marker 
